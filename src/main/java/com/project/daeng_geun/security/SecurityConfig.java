d5345b5d9d4ab09a026cd0bd45ae8c91f061054e
jinsuna <shs1401009@gmail.com>
2025-04-04 03:52:44 +0900

    rrrreeeeeee

--- src/main/java/com/project/daeng_geun/security/SecurityConfig.java
+++ src/main/java/com/project/daeng_geun/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://3.37.145.80")); // React 프론트엔드 허용
+        configuration.setAllowedOrigins(List.of("*")); // React 프론트엔드 허용
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
